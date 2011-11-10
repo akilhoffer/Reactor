ea25008dbe3bff4e46203d0c81f009453263b96c
Anthony Kilhoffer <anthony@kilhoffer.com>
2011-11-09 23:17:58 -0700

    Created SendEmailReaction. Stubbed only. Needs implementation and need to flesh out the interface.

---
+++ Reactor/ServiceGrid/Reactions/SendEmailReaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Reactor.Entities;
+
+namespace Reactor.ServiceGrid.Reactions
+{
+    public class SendEmailReaction : IReaction
+    {
+        public string Name { get; set; }
+
+        public void Execute()
+        {
+            
+        }
+    }
+}
